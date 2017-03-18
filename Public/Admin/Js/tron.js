140680b08d49e87a239e1ff83af8f1422d4ce160
mgs_ppstu <969060233@qq.com>
2017-03-18 16:20:02 +0800

    Revert "ppstu"
    This reverts commit 4913a977ba9f5ba5a02c35c34f49545f8b845284.

--- Public/Admin/Js/tron.js
+++
@@ -1,7 +0,0 @@
-
-  $('.tron').mouseover(function(){
-   $(this).find('td').css('backgroundColor','#158B7C');
- });
-$('.tron').mouseout(function(){
-   $(this).find('td').css('backgroundColor','#fff');
-});
